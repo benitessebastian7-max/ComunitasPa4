30004f2d36db5149f0edd45fbd978120a0310870
sebas <sebas@SEBASPRO1>
2026-07-13 20:40:33 -0500

    Add project files.

---
+++ FormIngreso.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace pe.com.muertelenta.ui
+{
+    partial class FormIngreso
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormIngreso));
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            btnIngresar = new Button();
+            btnSalir = new Button();
+            pictureBox1 = new PictureBox();
+            progressBar1 = new ProgressBar();
+            timer1 = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(27, 110);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Usuario:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(27, 183);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Clave:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(259, 9);
+            label3.Name = "label3";
+            label3.Size = new Size(314, 40);
+            label3.TabIndex = 2;
+            label3.Text = "INGRESO AL SISTEMA";
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(61, 4);
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(115, 107);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(285, 23);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(115, 180);
+            textBox2.Name = "textBox2";
+            textBox2.PasswordChar = '*';
+            textBox2.Size = new Size(285, 23);
+            textBox2.TabIndex = 5;
+            // 
+            // btnIngresar
+            // 
+            btnIngresar.Location = new Point(27, 274);
+            btnIngresar.Name = "btnIngresar";
+            btnIngresar.Size = new Size(75, 23);
+            btnIngresar.TabIndex = 6;
+            btnIngresar.Text = "Ingresar";
+            btnIngresar.UseVisualStyleBackColor = true;
+            btnIngresar.Click += btnIngresar_Click;
+            // 
+            // btnSalir
+            // 
+            btnSalir.Location = new Point(315, 274);
+            btnSalir.Name = "btnSalir";
+            btnSalir.Size = new Size(85, 23);
+            btnSalir.TabIndex = 7;
+            btnSalir.Text = "Salir";
+            btnSalir.UseVisualStyleBackColor = true;
+            btnSalir.Click += btnSalir_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(479, 72);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(301, 302);
+            pictureBox1.TabIndex = 8;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(27, 351);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(373, 23);
+            progressBar1.TabIndex = 9;
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // FormIngreso
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 415);
+            Controls.Add(progressBar1);
+            Controls.Add(pictureBox1);
+            Controls.Add(btnSalir);
+            Controls.Add(btnIngresar);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "FormIngreso";
+            TransparencyKey = Color.Red;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private ContextMenuStrip contextMenuStrip1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button btnIngresar;
+        private Button button1;
+        private Button btnSalir;
+        private PictureBox pictureBox1;
+        private ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
