401524d02887148c4f6532ee9f01e55f56a3fdac
rcanales1810 <160544301+rcanales1810@users.noreply.github.com>
2026-07-15 00:39:04 -0500

    Add DetalleVentaBO class with properties

---
+++ pe.com.communitas.bo/DetalleVentaBO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pe.com.communitas.bo
+{
+    public class DetalleVentaBO
+    {
+        public int iddetvent { get; set; }
+        public int idventa { get; set; }
+        public int idprod { get; set; }
+        public int canventa { get; set; }
+        public decimal precuventa { get; set; }
+        public decimal subtotventa { get; set; }
+
+      public VentaBO idventa { get; set; }
+      public ProductoBO idprod { get; set; }
+    }
+}
