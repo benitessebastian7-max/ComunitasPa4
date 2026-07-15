4798c219f60da686143efd712f4ad44ca1b0291e
rcanales1810 <160544301+rcanales1810@users.noreply.github.com>
2026-07-15 00:40:25 -0500

    Add DetalleCompraBO class for purchase details

---
+++ pe.com.communitas.bo/DetalleCompra.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pe.com.communitas.bo
+{
+    public class DetalleCompraBO
+    {
+        public int iddetcomp { get; set; }
+        public int idcompra { get; set; }
+        public int idprod { get; set; }
+        public int cancompra { get; set; }
+        public decimal precucomp { get; set; }
+        public decimal subtotcomp { get; set; }
+
+      public CompraBO idcompra { get; set; }
+      public ProductoBO idprod { get; set; }
+    }
+}
