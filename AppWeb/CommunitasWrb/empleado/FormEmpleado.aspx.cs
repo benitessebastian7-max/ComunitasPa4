56e1eb42b1742c1ac39d599f8a51c7bbb441beb3
benitessebastian7-max <benitessebastian7@gmail.com>
2026-07-17 18:42:10 -0500

    Add files via upload

---
+++ AppWeb/CommunitasWrb/empleado/FormEmpleado.aspx.cs
@@ -0,0 +1,45 @@
+﻿
+using pe.com.communitas.ui.util; 
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace pe.com.communitas.ui.empleado 
+{
+    public partial class FormEmpleado : System.Web.UI.Page
+    {
+
+        protected void btnNuevo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnRegistrar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnActualizar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnEliminar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnHabilitar_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("FormHabilitarEmpleado.aspx");
+        }
+
+        protected void gvEmpleado_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ AppWeb/CommunitasWrb/empleado/FormHabilitarEmpleado.aspx.cs
@@ -0,0 +1,34 @@
+﻿
+using pe.com.communitas.ui.util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace pe.com.communitas.ui.empleado
+{
+    public partial class FormHabilitarEmpleado : System.Web.UI.Page
+    {
+        protected void btnHabilitar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnDeshabilitar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnRegresar_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("FormEmpleado.aspx");
+        }
+
+        protected void gvHabilitarEmpleado_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+
+        }
+    }
+}
