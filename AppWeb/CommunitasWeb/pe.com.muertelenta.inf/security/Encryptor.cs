5a0da1214cb715e1508d0f7fb266f69192fa5401
benitessebastian7-max <benitessebastian7@gmail.com>
2026-07-17 19:01:28 -0500

    Add files via upload

---
+++ AppWeb/CommunitasWeb/pe.com.muertelenta.inf/security/Encryptor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pe.com.muertelenta.inf.security
+{
+    public static class Encryptor
+    {
+        //agregamos un metodo para cifrar un texto plano y convertirloa Base64
+        public static string Encrypt(string plaintext)
+        {
+            //convertimos el texto a un arreglo de bytes usando UTF8
+            byte[] data = Encoding.UTF8.GetBytes(plaintext);
+            //transformamos los bytes a una cadena Base64
+            return Convert.ToBase64String(data);
+        }
+
+        //agregamos un metodo para descifrar el texto convertido a base 64
+        public static string Decrypt(string ciphertext)
+        {
+            //convertimos la cadena Base64 a un arreglo de bytes
+            byte[] data = Convert.FromBase64String(ciphertext);
+            //transformamos los bytes nuevamente a texto usando UTF8
+            return Encoding.UTF8.GetString(data);
+        }
+
+    }
+}
---
+++ AppWeb/CommunitasWeb/pe.com.muertelenta.inf/security/PasswordHasher.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pe.com.muertelenta.inf.security
+{
+    public static class PasswordHasher
+    {
+        //generamos una funcion que permita un hash seguro de la contraseña usando algoritmo
+        //SAH256, esto ideal para almacenar contraseñas en una base e datos
+            public static string HashPassword(string password)
+            {
+                using (var sha = SHA256.Create())
+                {
+                    //convertimos la contraseña a bytes usando UTF8
+                    var bytes = Encoding.UTF8.GetBytes(password);
+                    //calculamos el hash de los bytes
+                    var hash = sha.ComputeHash(bytes);
+                    //convertimos el hash en una cadena hexadecimal en minusculas
+                    return BitConverter.ToString(hash).Replace("-", "").ToLower();
+                }
+            }
+
+            //creamos una funcion para verificar si una contraseña ingresada coincide con el hash
+            //almacenado, se vuelve a hashear la contraseña ingresada
+            public static bool VerifyPassword(string password, string hash)
+            {
+                //generamos el hash de la contraseña ingresada
+                string newhash = HashPassword(password);
+                //comparamos el hash generado con el has almacenado
+                return newhash == hash;
+            }
+        }
+
+    }
+
